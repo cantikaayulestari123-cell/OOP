bd9d7cb24b01fb30340e423b817e39584d939a3a
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-21 18:53:29 +0700

    menambahkan prosedurOOP

--- new.cpp
+++ new.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
 using namespace std;
+
+class barang{
+    public:
+        string nama;
+        int jumlah;
+        string kategori;
+        string tanggalProduksi;
+        void tampilSpek(){
+            cout << "nama barang : " << nama << endl;
+            cout << "jumlah barang : " << jumlah << endl;
+            cout << "kategori barang : " << kategori << endl;
+            cout << "tanggal produksi : " << tanggalProduksi << endl;
+        } 
+};
+int main(){
+    barang elektronik;
+    barang nonElectronik;
+
+    elektronik.nama = "Laptop";
+    elektronik.jumlah = 1;
+    elektronik.kategori = "elektronik";
+    elektronik.tanggalProduksi = "1945-08-17";
+    elektronik.tampilSpek();
+
+    cout << endl;
+
+    nonElectronik.nama = "supersemar";
+    nonElectronik.jumlah = 1;
+    nonElectronik.kategori = "nonElektronik";
+    nonElectronik.tampilSpek();
+}
+    
+
+
+
